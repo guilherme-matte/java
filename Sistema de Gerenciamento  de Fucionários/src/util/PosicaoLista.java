b558728e49311a89399e46670ce04d1005cbea68
guilherme-matte <guiguimatte@gmail.com>
2024-09-09 17:54:44 -0300

    att

---
+++ Sistema de Gerenciamento  de Fucionários/src/util/PosicaoLista.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.awt.Desktop;
+import javax.swing.JDesktopPane;
+import javax.swing.JInternalFrame;
+
+/**
+ *
+ * @author guilherme-matte
+ */
+public class PosicaoLista {
+
+    public void abrirTelaCentro(JInternalFrame tela, JDesktopPane jdp) {
+        int lDesk = jdp.getWidth();
+        int aDesk = jdp.getHeight();
+        int lIframe = tela.getWidth();
+        int aIframe = tela.getHeight();
+        tela.setLocation(lDesk / 2 - lIframe / 2, aDesk / 2 - aIframe / 2);
+        jdp.add(tela);
+        tela.setVisible(true);
+    }
+}

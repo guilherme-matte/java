d03f26e3a874ca61caea41e94ee54d4f0fc10854
guilherme-matte <guiguimatte@gmail.com>
2024-11-27 17:59:02 -0300

    Update RateController.java

--- springboot/src/main/java/com/example/springboot/controller/RateController.java
+++ springboot/src/main/java/com/example/springboot/controller/RateController.java
@@ -1,32 +1,45 @@
 package com.example.springboot.controller;
 
-import com.example.springboot.models.ProductModel;
 import com.example.springboot.models.RateModel;
-import com.example.springboot.repositories.ProductRepository;
 import com.example.springboot.repositories.RateRepository;
-import jakarta.validation.Valid;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 public class RateController {
     @Autowired
     RateRepository rateRepository;
+
     @GetMapping("/products/{id}/rate")
-    public ResponseEntity<Object> getRateByProductId(@PathVariable(value = "id")String idProduct){
+    public ResponseEntity<Object> getRateByProductId(@PathVariable(value = "id") String idProduct) {
         Optional<RateModel> rate = rateRepository.findByIdProduct(idProduct);
         System.out.println(rate);
-        if (rate.isPresent()){
+        if (rate.isPresent()) {
             return ResponseEntity.status(HttpStatus.OK).body(rate.get());
-        }else{
+        } else {
             System.out.println(idProduct);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR - 1371");
         }
-            }
+    }
+
+    @PutMapping("/rate/{id}")
+    public ResponseEntity<Object> insertRate(@PathVariable(value = "id") String idProduct, @RequestBody RateModel newRateData) {
+        Optional<RateModel> rate = rateRepository.findByIdProduct(idProduct);
+        System.out.println(rate);
+        if (rate.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR - 1372");
+        } else {
+            RateModel rateModel = rate.get();
+
+            rateModel.setQuantity(newRateData.getQuantity());
+            rateModel.setAllRate(newRateData.getAllRate());
+
+            rateRepository.save(rateModel);
+            return ResponseEntity.status(HttpStatus.OK).body(rateModel);
+        }
+    }
 }

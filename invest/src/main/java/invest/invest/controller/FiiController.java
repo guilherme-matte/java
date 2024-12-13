46a101bfb0f9cc1303e9131104b5206e88a5a79d
guilherme-matte <guiguimatte@gmail.com>
2024-12-13 18:51:19 -0300

    Atualizar o FiiController.java

--- invest/src/main/java/invest/invest/controller/FiiController.java
+++ invest/src/main/java/invest/invest/controller/FiiController.java
@@ -45,7 +45,7 @@ public class FiiController {
     @GetMapping("/get/{siglaFii}")
     public ResponseEntity<Object> getOneFii(@PathVariable(value = "siglaFii") String siglaFii) {
         Optional<FiiModel> fii = fiiRepository.findBySiglaFii(siglaFii.toUpperCase());
-var valorCota = ValorCota
+var valorCota = CalcularValorCota.calcularCota(fiiRepository.getPl(),fiiRepository.getNumCotas());
         try {
             if (fii.isPresent()) {
                 return ResponseEntity.status(HttpStatus.OK).body(fii.get());

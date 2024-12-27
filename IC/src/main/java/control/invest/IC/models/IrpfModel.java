d178bc199626c09ec0f4c8f56701cc20fc1b4554
guilherme-matte <guiguimatte@gmail.com>
2024-12-26 22:46:25 -0300

    criação projeto IC

---
+++ IC/src/main/java/control/invest/IC/models/IrpfModel.java
@@ -0,0 +1,42 @@
+package control.invest.IC.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_irpf")
+public class IrpfModel extends RepresentationModel<IrpfModel> implements Serializable {
+
+    //classe para fins de teste, futuramente serão adicionados outros tipos de rendimentos
+
+    private UUID idIrpf;
+
+    private String cpf;
+    private String nomePessoaFisica;
+
+    private String fontePagadoraCnpj;
+    private String fontePagadoraNomeEmpresa;
+
+    //rendimentos tributaveis
+    private double rendimentosTotais;
+    private double prevSocial;
+    private double impostoRetido;
+
+    // rendimentos isentos
+    private double pagamentosRecebidos;//Pagamento realizado ao titular ou socio de micro empresa
+    private double outrosRendimentosIsentos;
+
+
+    // rendimentos exclusivos
+    private double decTercSal;
+
+    //rendimentos acumulados
+    private double pensao;
+
+
+
+}
